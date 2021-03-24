408b555ef9ef33cd82dd648d26da5ea645ce02d1
Vladimir <vxvxn@yandex.ru>
2021-03-24 22:00:34 +0300

    add tests

---
+++ log_test.go
@@ -0,0 +1,112 @@
+package log
+
+import (
+	"io/ioutil"
+	"os"
+	"strings"
+	"testing"
+)
+
+var (
+	traceText   = "trace text"
+	debugText   = "debug text"
+	infoText    = "info text"
+	warningText = "warning text"
+	errorText   = "error text"
+	fatalText   = "fatal text"
+)
+
+func TestInitCommonMode(t *testing.T) {
+	nameFile := "commonLog.log"
+	defer os.Remove(nameFile)
+
+	if err := Init(nameFile, CommonLog, false); err != nil {
+		t.Errorf("Init() error = %v", err)
+	}
+
+	Trace.Println(traceText)
+	Debug.Println(debugText)
+	Info.Println(infoText)
+	Warning.Println(warningText)
+	Error.Println(errorText)
+	Fatal.Println(fatalText)
+
+	expectedLogs := []string{
+		infoText,
+		warningText,
+		errorText,
+		fatalText,
+	}
+
+	unexpectedLogs := []string{
+		traceText,
+		debugText,
+	}
+
+	file, err := os.Open(nameFile)
+	if err != nil {
+		t.Errorf("Open() error = %v", err)
+	}
+	defer file.Close()
+
+	dataBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		t.Errorf("Open() error = %v", err)
+	}
+	data := string(dataBytes)
+
+	for _, expectedLog := range expectedLogs {
+		if !strings.Contains(data, expectedLog) {
+			t.Errorf("Incorrect logs, expected to find: %v in  \n%v", expectedLog, data)
+		}
+	}
+
+	for _, unexpectedLog := range unexpectedLogs {
+		if strings.Contains(data, unexpectedLog) {
+			t.Errorf("Incorrect logs, unexpected to find: %v in \n%v", unexpectedLog, data)
+		}
+	}
+}
+
+func TestInitTraceMode(t *testing.T) {
+	nameFile := "traceLog.log"
+	defer os.Remove(nameFile)
+
+	if err := Init(nameFile, TraceLog, false); err != nil {
+		t.Errorf("Init() error = %v", err)
+	}
+
+	Trace.Println(traceText)
+	Debug.Println(debugText)
+	Info.Println(infoText)
+	Warning.Println(warningText)
+	Error.Println(errorText)
+	Fatal.Println(fatalText)
+
+	expectedLogs := []string{
+		traceText,
+		debugText,
+		infoText,
+		warningText,
+		errorText,
+		fatalText,
+	}
+
+	file, err := os.Open(nameFile)
+	if err != nil {
+		t.Errorf("Open() error = %v", err)
+	}
+	defer file.Close()
+
+	dataBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		t.Errorf("Open() error = %v", err)
+	}
+	data := string(dataBytes)
+
+	for _, expectedLog := range expectedLogs {
+		if !strings.Contains(data, expectedLog) {
+			t.Errorf("Incorrect logs, expected to find: %v in  \n%v", expectedLog, data)
+		}
+	}
+}
